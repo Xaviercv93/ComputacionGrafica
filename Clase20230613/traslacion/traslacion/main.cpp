9d90b7fc22d10b32ed1ea5306d88af1f0838836f
JavierCriollo <javier.criollo@epn.edu.ec>
2023-06-13 08:14:39 -0500

    CUBOS sin rotar sin correccion de profundidad

---
+++ Clase20230613/traslacion/traslacion/main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <GL/freeglut.h>
+
+void reshape(GLint w, GLint h);
+void initGL();
+void display3D();
+void dibujarCubo();
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Ejercicio Animaciones 3D");
+
+    glutDisplayFunc(display3D);
+    glutReshapeFunc(reshape);
+    initGL();
+    glutMainLoop();
+
+}
+
+void reshape(GLint w, GLint h) {
+    glViewport(0, 0, w, h);
+    //glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0f, 1, 5, 100);
+}
+
+//es mejor tenerlo como funcion porque luego usamos para inicializar varias cosas, como la correccion de profundidad
+//aqui se hara las correcciones iniciales de los buffer
+void initGL() {
+    glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+    //glMatrixMode(GL_PROJECTION);
+    //glLoadIdentity();
+    //gluPerspective(45.0f, 1, 5, 100); ya se llama en el reshape
+}
+
+void display3D() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(9, 9, 9, 0, 0, 0, 0, 1, 0); //camara en 5,5,5 apunta al 0,0,0 y el vector hacia arriba es 0,1,0
+
+    //grafico objeto nuevo
+    //para colocar en coordenadas de mundo toca trasladar
+
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, -6.0f); //la mando al fondo, esto afecta a las 2, para solucionarlo usar push y pop
+    dibujarCubo();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glScalef(1.5f, 1.5f, 1.5f);//escalamos para ver la diferencia por tamaño entre los 2 cubos 
+    dibujarCubo();
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+
+//en teoria no importa en que orden coloco las caras, la correccion de profundidad nos ayuda
+void dibujarCubo()
+{
+    glBegin(GL_QUADS);//SIEMPRE colocar porque aqui defino primitivas
+    //primero definimos en coordenadas locales
+    //luego lo trasladamos para ponerlo en coordenadas del mundo
+
+
+    //color primera cara
+    glColor3f(0.9f, 0.8f, 0.7f);
+    //defino 4 vertices para 1 cara , la lateral izquierda
+    //defino en coordenadas locales
+    //tambien pueden se posiciones de mundo siempre y cuando esten dentro de la perspectiva segun esto la perpectiva va de 1 a 5  en gluPerspective(60.0f, 1, 5, 100);
+
+    //otra cara, lateral izquierda
+    glVertex3f(-1, -1, -1);
+    glVertex3f(-1, -1, 1);
+    glVertex3f(-1, 1, 1);
+    glVertex3f(-1, 1, -1);
+
+    //otra cara, inferior
+    glColor3f(0.0f, 0.5f, 1.0f);
+
+    glVertex3f(-1, -1, -1);
+    glVertex3f(-1, -1, 1);
+    glVertex3f(1, -1, 1);
+    glVertex3f(1, -1, -1);
+
+
+    //otra cara, posterior
+    glColor3f(0.95f, 0.8f, 0.4f);
+    glVertex3f(-1, -1, -1);
+    glVertex3f(-1, 1, -1);
+    glVertex3f(1, 1, -1);
+    glVertex3f(1, -1, -1);
+
+    //otra cara, superior
+    glColor3f(0.85f, 0.25f, 0.4f);
+
+    glVertex3f(-1, 1, -1);
+    glVertex3f(-1, 1, 1);
+    glVertex3f(1, 1, 1);
+    glVertex3f(1, 1, -1);
+
+
+    //otra cara, frontal
+    glColor3f(0.0f, 0.1f, 0.7f);
+
+    glVertex3f(-1, -1, 1);
+    glVertex3f(-1, 1, 1);
+    glVertex3f(1, 1, 1);
+    glVertex3f(1, -1, 1);
+
+
+    //otra cara, lateral derecha
+    glColor3f(1.0f, 0.5f, 0.0f);
+
+    glVertex3f(1, -1, -1);
+    glVertex3f(1, -1, 1);
+    glVertex3f(1, 1, 1);
+    glVertex3f(1, 1, -1);
+
+
+    glEnd();
+}
--- Deber2-JavierCriollo/Tarea02_Circulos2D/Tarea02_Circulos2D/Main.cpp
+++ Deber2-JavierCriollo/Tarea02_Circulos2D/Tarea02_Circulos2D/Main.cpp
@@ -1,113 +1,86 @@
-﻿//incluye la biblioteca freeglut que proporciona funciones para crear y administrar ventanas y gráficos en OpenGL.
-#include<GL/freeglut.h>
-//incluye la biblioteca math.h que proporciona funciones matemáticas, como sin y cos
-#include<math.h>
-//Declara las variables x, y e i como tipo de dato float
-float x, y, i;
-//Declara la variable PI y le asigna el valor de π (pi) aproximado a 3.1416
-float PI = 3.1416;
-//Define una función llamada circle sin argumentos ni valor de retorno.
-void circle(void)
+﻿#include<GL/freeglut.h>     //incluye la biblioteca freeglut que proporciona funciones para crear y administrar ventanas y gráficos en OpenGL.
+#include<math.h>            //incluye la biblioteca math.h que proporciona funciones matemáticas, como sin y cos
+
+float x, y, i;              
+float PI = 3.1416;          
+
+void circulo(void)
 {
-    //Declara las variables x e y como tipo de dato float dentro de la función circle. 
-    //Estas variables se utilizarán para representar las coordenadas del centro del círculo en cada iteración.
-    float x, y;
-    //Declara la variable i como tipo de dato int dentro de la función circle. 
-    //Esta variable se utilizará en los bucles para iterar.
-    int i;
-    //Declara la variable radius como tipo de dato GLfloat dentro de la función circle. 
-    //Esta variable se utilizará para especificar el radio del círculo.
-    GLfloat radius;
-    //Declara la variable triangleAmount y le asigna el valor de 40. 
-    //Esta variable determina la cantidad de triángulos utilizados para aproximar el círculo.
-    int triangleAmount = 40;
-    //Declara la variable twicePi y le asigna el valor de 2.0 * PI. 
-    //Esta variable se utiliza para calcular los puntos en el círculo utilizando la fórmula trigonométrica
-    GLfloat twicePi = 2.0 * PI;
+    glClearColor(0.1, 0.1, 0.1, 0.1);    //Establece el color de fondo de la ventana en negro utilizando los valores RGB.
+    glClear(GL_COLOR_BUFFER_BIT);        //limpiar el búfer de color
+
+    float x, y;             //Estas variables se utilizarán para representar las coordenadas del centro del círculo en cada iteración.
+
+    int i;                  //Declara la variable i se utilizará en los bucles para iterar.
+
+    GLfloat radio;         //Esta variable se utilizará para especificar el radio del círculo, es tipo de dato GLfloat
+
+    int numeroTriangulos = 100;//Esta variable determina la cantidad de triángulos utilizados para aproximar el círculo.
+
+    GLfloat doblePi = 2.0 * PI;//Esta variable se utiliza para calcular los puntos en el círculo utilizando la fórmula trigonométrica
 
 
-    //Inicia la definición de un nuevo primitivo gráfico, en este caso, un TRIANGLE_FAN
+    //primer circulo
+    glBegin(GL_TRIANGLE_FAN);    //Inicia la definición de un nuevo primitivo gráfico, en este caso, un TRIANGLE_FAN
+        glColor3ub(0, 0, 255);    //Establece el color actual de los vértices a un tono de azul utilizando los valores RGB.
+        x = 0; y = 0; radio = 20;    //Asigna los valores adecuados a las variables x, y y radio para el primer círculo.
+
+        doblePi = 2.0 * PI;
+
+        glVertex2f(x, y);     //Es el centro del círculo. Agrega un vértice en las coordenadas (x, y) al TRIANGLE_FAN. 
+        for (i = 0; i <= numeroTriangulos;i++) {
+            //Agrega vértices adicionales al TRIANGLE_FAN, calculados utilizando las funciones trigonométricas cos y sin.
+            //esto especifica las coordenadas x e y del vértice en el espacio tridimensional.
+            glVertex2f(
+                x + (radio * cos(i * doblePi / numeroTriangulos)),
+                y + (radio * sin(i * doblePi / numeroTriangulos))
+            );
+        }
+
+
+    glEnd();    //Finaliza la definición del primitivo gráfico.
+
+    //segundo circulo
     glBegin(GL_TRIANGLE_FAN);
-    //Establece el color actual de los vértices a un tono de naranja utilizando los valores RGB.
-    glColor3ub(238, 139, 21);
-    //Asigna los valores adecuados a las variables x, y y radius para el primer círculo.
-    x = 0; y = 0; radius = 20;
+        glColor3ub(255, 255, 255);
+        x = 50; y = 40; radio = 40;
 
+        doblePi = 2.0 * PI;
 
-
-    
-    twicePi = 2.0 * PI;
-
-
-    //Agrega un vértice en las coordenadas (x, y) al abanico de triángulos. En este caso, representa el centro del círculo.
-    //centro del circulo
-    glVertex2f(x, y); 
-    //Inicia un bucle que itera desde 0 hasta triangleAmount, incrementando i en cada iteración.
-    for (i = 0; i <= triangleAmount;i++) {
-        //Agrega vértices adicionales al abanico de triángulos, calculados utilizando las funciones trigonométricas cos y sin.
-        glVertex2f(
-            x + (radius * cos(i * twicePi / triangleAmount)),
-            y + (radius * sin(i * twicePi / triangleAmount))
-        );
-    }
-
-
-    //Finaliza la definición del primitivo gráfico.
-    glEnd();
-
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3ub(199, 194, 187);
-    x = -50; y = 50; radius = 20;
-
-
-
-
-    twicePi = 2.0 * PI;
-
-
-
-    glVertex2f(x, y); // center of circle
-    for (i = 0; i <= triangleAmount;i++) {
-        glVertex2f(
-            x + (radius * cos(i * twicePi / triangleAmount)),
-            y + (radius * sin(i * twicePi / triangleAmount))
-        );
-    }
-
-
-
+        glVertex2f(x, y); //Es el centro del círculo. 
+        for (i = 0; i <= numeroTriangulos;i++) {
+            glVertex2f(
+                x + (radio * cos(i * doblePi / numeroTriangulos)),
+                y + (radio * sin(i * doblePi / numeroTriangulos))
+            );
+        }
     glEnd();
 
 
-    //Intercambia los buffers de la ventana, mostrando el resultado en pantalla.
-    glutSwapBuffers();
+    glutSwapBuffers();    //Intercambia los buffers de la ventana, mostrando el resultado en pantalla.
+
 }
-//Define la función init sin argumentos ni valor de retorno.
+
 void init(void)
 {
-    //Establece el color de fondo de la ventana en blanco utilizando los valores RGB.
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    //Reemplaza la matriz actual con la matriz identidad.
-    glLoadIdentity();
-    //Establece la matriz de proyección ortográfica para definir el volumen de visualización.
-    glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
+   
+
+    glLoadIdentity();    //Reemplaza la matriz actual con la matriz identidad.
+    glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);    //Establece la matriz de proyección ortográfica para definir el volumen de visualización.
+
 }
 int main(int argc, char** argv)
 {
-    //Inicializa la biblioteca GLUT.
-    glutInit(&argc, argv);
-    //Establece el modo de visualización de la ventana, en este caso, utilizando el modelo de color RGB y buffers dobles.
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-    //Establece el tamaño de la ventana.
-    glutInitWindowSize(500, 550);
-    //Crea una ventana con el título "Circle".
-    glutCreateWindow("Circle");
-    //Establece la función circle como la función de visualización.
-    glutDisplayFunc(circle);
-    //Llama a la función init para realizar la inicialización adicional.
-    init();
-    //Inicia el bucle principal de GLUT.
-    glutMainLoop();
-    //Retorna 0 para indicar que el programa se ha ejecutado correctamente.
-    return 0;
+    glutInit(&argc, argv);    //Inicializa la biblioteca GLUT.
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);    //Establece el modo de visualización de la ventana, en este caso, utilizando el modelo de color RGB y buffers dobles.
+    glutInitWindowSize(500, 550);    //Establece el tamaño de la ventana.
+    glutCreateWindow("Circulos");    //Crea una ventana con el título "Circulos".
+    glutDisplayFunc(circulo);    //Establece la función circle como la función de visualización.
+    init();    //Llama a la función init para realizar la inicialización adicional.
+
+    glutMainLoop();    //Inicia el bucle principal de GLUT.
+
+    return 0;    //Retorna 0 para indicar que el programa se ha ejecutado correctamente.
+
 }
