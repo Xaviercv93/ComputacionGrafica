b66b1c5793a6921704816762ae588255e9225bc6
JavierCriollo <javier.criollo@epn.edu.ec>
2023-06-19 08:02:46 -0500

    rotacion y traslacion controlado por teclado

--- Clase20230613/traslacion/traslacion/main.cpp
+++ Clase20230613/traslacion/traslacion/main.cpp
@@ -8,9 +8,21 @@ void display3D();
 void dibujarCubo();
 void timer(int valor);
 
+void keyDown(int key, int, int);
+void keyUp(int key, int, int);
+
+
+void spinDisplayIzq();
+void spinDisplayDer();
+
 int refreshRate = 5;//variable tipica de los scripts
 
 float rotAng = 0.0f;
+
+bool iskey = false, ismouse = false;//verifica si la tecla esta presionada con iskey
+bool isFirts = true;
+float posx = 0.0f;
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
@@ -22,7 +34,11 @@ int main(int argc, char** argv)
     glutDisplayFunc(display3D);//no se puede mandar paramatros porque es puntero funcion
     glutReshapeFunc(reshape);
 
-    glutTimerFunc(0, timer, 0);//el tiempo en el que llame
+    glutSpecialFunc(keyDown);
+    glutSpecialUpFunc(keyUp);
+
+    //para que funcione con teclado paramos el timer y redibujar
+    //glutTimerFunc(0, timer, 0);//el tiempo en el que llame
 
     initGL();
     glutMainLoop();
@@ -45,6 +61,7 @@ void reshape(GLint w, GLint h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(60.0f, 1, 5, 100);
+    //timer(10);
 }
 
 //es mejor tenerlo como funcion porque luego usamos para inicializar varias cosas, como la correccion de profundidad
@@ -54,36 +71,104 @@ void initGL() {
     //glMatrixMode(GL_PROJECTION);
     //glLoadIdentity();
     //gluPerspective(45.0f, 1, 5, 100); ya se llama en el reshape
+
+
+    //prueba de profundiad para ver las caras
+    glEnable(GL_DEPTH_TEST);
+}
+
+void spinDisplayIzq() {
+    //calculamos la posicion y luego hacemos un translate
+    //si esta presionado la tecla
+    if (iskey)
+        posx -= 0.05f;
+    //hago un redisplay
+    glutPostRedisplay();
+
+
+}
+void spinDisplayDer() {
+    //calculamos la posicion y luego hacemos un translate
+    //si esta presionado la tecla
+    if (iskey)
+        posx += 0.05f;
+    //hago un redisplay
+    glutPostRedisplay();
+
+
+}
+void keyDown(int key, int, int) {
+    switch (key) {
+    case GLUT_KEY_LEFT:
+
+        //verifico si esta presionado la tecla
+        iskey = true;
+        glutIdleFunc(spinDisplayIzq);//Mantengo mi condicion de parada
+        break;
+    case GLUT_KEY_RIGHT:
+        //verifico si esta presionado la tecla
+        iskey = true;
+        glutIdleFunc(spinDisplayDer);
+        break;
+    default:
+        break;
+    }
+}
+void keyUp(int key, int, int) {
+    switch (key) {
+    case GLUT_KEY_LEFT:
+
+        //verifico si esta presionado la tecla
+        iskey = false;
+        glutIdleFunc(NULL);//Mantengo mi condicion de parada
+        break;
+    case GLUT_KEY_RIGHT:
+        //verifico si esta presionado la tecla
+        iskey = false;
+        glutIdleFunc(NULL);
+        break;
+    default:
+        break;
+    }
 }
 
 void display3D() {//no se puede mandar paramatros porque es puntero funcion
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0, 9, 9, 0, 0, 0, 0, 1, 0); //camara en 5,5,5 apunta al 0,0,0 y el vector hacia arriba es 0,1,0
+    gluLookAt(9, 9, 9, 0, 0, 0, 0, 1, 0); //camara en 5,5,5 apunta al 0,0,0 y el vector hacia arriba es 0,1,0
 
     //grafico objeto nuevo
     //para colocar en coordenadas de mundo toca trasladar
 
     glPushMatrix();
-    glTranslatef(0.0f, 0.0f, -6.0f); //la mando al fondo, esto afecta a las 2, para solucionarlo usar push y pop
-    glRotatef(45.0f, 0, 0, 1);
+    if (iskey) {
+        glTranslatef(posx, 0, -6.0f);
+    }
+    else {
+        //este translate solo se necesita la 1ra vez para posicionar el cubo, luego ya no
+        glTranslatef(0.0f, 0.0f, -6.0f); //la mando al fondo, esto afecta a las 2, para solucionarlo usar push y pop
+    }
+    glRotatef(-rotAng, 0, 0, 1);
     dibujarCubo();
     glPopMatrix();
 
+
     glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(-3.0f, 0.0f, 0.0f);
     glScalef(1.5f, 1.5f, 1.5f);//escalamos para ver la diferencia por tamaño entre los 2 cubos 
     //glRotatef(45.0f, 0, 0, 1);
     glRotatef(rotAng, 0, 0, 1);
-
     dibujarCubo();
     glPopMatrix();
 
     glutSwapBuffers();
-    rotAng += 0.1f;
+    rotAng += 0.005f;
     //display3D();//para no dar click
     //glutPostRedisplay();
+
+
+    //isFirts = false;
 }
 
 
